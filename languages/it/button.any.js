00b057b857c7e17395f99b97f88d3a4f5b484739
Drijon7577 <87547203+Drijon7577@users.noreply.github.com>
2022-05-06 22:39:37 +0200

    Pronouns Any Button
    The Button for any Pronouns

---
+++ languages/it/button.any.js
@@ -0,0 +1,4 @@
+module.exports = {
+    noData: 'Impossibile trovare il server nel database!',
+    roleHierarchy: 'Il\' mio Roulo non è stato messo sopra {role}, non sono in grado di fare niente '
+};
