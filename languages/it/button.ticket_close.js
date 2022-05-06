b88c6b4ea0c95d2b14ff3ae0a784860fa2676d13
Drijon7577 <87547203+Drijon7577@users.noreply.github.com>
2022-05-06 22:59:42 +0200

    Create button.ticket_close.js

---
+++ languages/it/button.ticket_close.js
@@ -0,0 +1,15 @@
+module.exports = {
+    noData: 'Non ho trovato il Server',
+    confirm: {
+        text: 'Sei sicuro di volere Eliminare il canale {channel} per sempre?',
+        configReason: 'Per `motivo`, perfavore configura in => `/config` => *Ticketing*!',
+        button: {
+            close: 'Quisura Ticket',
+            closeReason: 'Quidi Ticket con un motivo'
+        }
+    },
+    form: {
+        formTitle: `Qiusura {channel}`,
+        reason: 'Motivo'
+    }
+};
