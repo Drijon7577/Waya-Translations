66c032d06780090ffab00a7d0406322f4e1a797c
Drijon7577 <87547203+Drijon7577@users.noreply.github.com>
2022-05-06 23:01:37 +0200

    Create button.ticket_create.js

---
+++ languages/it/button.ticket_create.js
@@ -0,0 +1,5 @@
+module.exports = {
+    noData: 'Non ho trovato il Server nel Database!',
+    noCategory: 'La categoria Tickets è stata eliminata. Si prega di avvisare un amministratore del server.',
+    created: 'Il tuo Ticket {channel} è stato creato.'
+};
